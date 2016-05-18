93a82d940c675e775cd38dc81f9fad0189c125f7
伟哥 <564114648@qq.com>
2016-05-18 10:26:09 +0800

    ..000

---
+++ text/text/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  text
+//
+//  Created by nevermore on 16/5/18.
+//  Copyright © 2016年 hzw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        self.view.backgroundColor=UIColor.whiteColor()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
